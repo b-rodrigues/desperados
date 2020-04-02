bedd4e415c54514bae7e2b6dfe7028cf78da0488
Bruno Rodrigues <bruno@brodrigues.co>
2020-04-02 23:21:53 +0200

    added random text

--- R/mod_about_module.R
+++ R/mod_about_module.R
@@ -4,27 +4,30 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_about_module_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+      h2("A Random Text"),
+      tableOutput(ns("text"))
   )
 }
-    
+
 #' about_module Server Function
 #'
-#' @noRd 
+#' @noRd
 mod_about_module_server <- function(input, output, session){
   ns <- session$ns
- 
+  output$text <- renderText({
+      shinipsum::random_text(nwords = 50)
+  })
 }
-    
+
 ## To be copied in the UI
 # mod_about_module_ui("about_module_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_about_module_server, "about_module_ui_1")
- 
+
