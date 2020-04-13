f4d4edb94468147903bccb3ff25d78f5fdb8c1c4
Bruno Rodrigues <bruno@brodrigues.co>
2020-04-13 16:49:24 +0200

    echarts bar plot

--- R/mod_plot_of_data.R
+++ R/mod_plot_of_data.R
@@ -7,25 +7,45 @@
 #' @noRd
 #' @importFrom dygraphs dygraphOutput renderDygraph
 #' @importFrom shiny NS tagList
+#' @importFrom echarts4r echarts4rOutput
 mod_plot_of_data_ui <- function(id){
   ns <- NS(id)
   tagList(
     box(title = "Controls",
         sliderInput(ns("slider"), "Number of obs:", 1,500,100),
         width = 2 ),
-    box(dygraphOutput(ns("my_histogram")))
+    box(echarts4rOutput(ns("my_histogram")))
 )
 }
 
 #' plot_of_data Server Function
 #' @importFrom shinipsum random_ggplot random_dygraph
 #' @importFrom dygraphs dygraph renderDygraph
+#' @importFrom dplyr mutate count sample_n
+#' @importFrom tidyr pivot_wider
+#' @importFrom forcats fct_lump
+#' @importFrom echarts4r renderEcharts4r e_charts e_bar
 #' @noRd
 mod_plot_of_data_server <- function(input, output, session){
   ns <- session$ns
-  output$my_histogram <- renderDygraph({
-      trash <- c(input$slider)
-      random_dygraph("histogram")
+  gss_cat <- forcats::gss_cat
+
+  counts_marital_race <- reactive({
+      gss_cat %>%
+      sample_n(size =  as.numeric(input$slider)) %>%
+      mutate(marital = fct_lump(marital, prop = 0.1)) %>%
+      count(marital, race)
+  })
+
+  output$my_histogram <- renderEcharts4r({
+    #trash <- c(input$slider)
+    counts_marital_race() %>%
+                           pivot_wider(names_from = race, values_from = n) %>%
+                           e_charts(marital) %>%
+                           e_bar(Other, stack = "grp") %>%
+                           e_bar(Black, stack = "grp") %>%
+                           e_bar(White, stack = "grp")
+      #random_dygraph("histogram")
       })
 }
 
@@ -34,4 +54,6 @@ mod_plot_of_data_server <- function(input, output, session){
 
 ## To be copied in the server
 # callModule(mod_plot_of_data_server, "plot_of_data_ui_1")
+
+
 
